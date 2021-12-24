ff709e7e72ae5816001beacbfcc3fd7dbfc724a5
miuhuru <zermoun.yasmine@gmail.com>
2021-12-24 20:23:15 +0100

    converted all functions expressions into arrow functions

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 "use strict";
-const addButton = document.querySelector(".add-btn")
-const subtractButton = document.querySelector(".subtract-btn")
-const multiplyButton = document.querySelector(".multiply-btn")
-const divideButton = document.querySelector(".divide-btn")
+
 
 let a 
 let b 
 
-
-const add = (a, b) => { return a + b }
-const subtract = (a, b) => { return a - b }
-const multiply = (a, b) => { return a * b }
-const divide = (a, b) => { return a / b }
-
+const add = (a, b) => { console.log(`Executing ${a} + ${b}`); return a + b }
+const subtract = (a, b) => { console.log(`Executing ${a} - ${b}`); return a - b }
+const multiply = (a, b) => { console.log(`Executing ${a} * ${b}`); return a * b }
+const divide = (a, b) => { console.log(`Executing ${a} / ${b}`); return a / b }
 
 
-addButton.addEventListener("click", add)
+
+
+
+/* const addButton = document.querySelector(".add-btn")
+const subtractButton = document.querySelector(".subtract-btn")
+const multiplyButton = document.querySelector(".multiply-btn")
+const divideButton = document.querySelector(".divide-btn")
+ addButton.addEventListener("click", add)
 subtractButton.addEventListener("click", subtract)
 multiplyButton.addEventListener("click", multiply)
-divideButton.addEventListener("click", divide)
+divideButton.addEventListener("click", divide) */
 
 
