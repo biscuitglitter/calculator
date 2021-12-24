5f2d660677f2564d48da596452352387032eacfa
miuhuru <zermoun.yasmine@gmail.com>
2021-12-24 22:02:56 +0100

    stuck. cant get the values of input a & b

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
 "use strict";
 
+const addButton = document.querySelector(".add-btn");
+const subtractButton = document.querySelector(".subtract-btn");
+const multiplyButton = document.querySelector(".multiply-btn");
+const divideButton = document.querySelector(".divide-btn");
 
-let a 
-let b 
-let result
+const displaySpace = document.getElementById("display");
 
-const add = (a, b) => { return a + b }
-const subtract = (a, b) => {  return a - b }
-const multiply = (a, b) => { return a * b }
+
+let a;
+let b;
+let result;
+var inputA
+var inputB
+
+const add = (a, b) => {
+  return a + b;
+};
+const subtract = (a, b) => {
+  return a - b;
+};
+const multiply = (a, b) => {
+  return a * b;
+};
 
 const divide = (a, b) => {
   if (a === 0 || b === 0) {
-    return "Numbers can't be divided by 0. Please try again"
-  } else 
-  return a / b
-}
+    return "Numbers can't be divided by 0. Please try again";
+  } else return a / b;
+};
 
 function operate(a, b, operator) {
   if (operator === "+") {
-    return add(a, b)
+    return add(a, b);
   } else if (operator === "-") {
-    return subtract(a, b)
+    return subtract(a, b);
   } else if (operator === "*") {
-    return multiply(a, b)
+    return multiply(a, b);
   } else if (operator === "/") {
-    return divide(a, b)
+    return divide(a, b);
   }
 }
 
 
 
-
-/* const addButton = document.querySelector(".add-btn")
-const subtractButton = document.querySelector(".subtract-btn")
-const multiplyButton = document.querySelector(".multiply-btn")
-const divideButton = document.querySelector(".divide-btn")
- addButton.addEventListener("click", add)
-subtractButton.addEventListener("click", subtract)
-multiplyButton.addEventListener("click", multiply)
-divideButton.addEventListener("click", divide) */
-
+addButton.addEventListener("click", add);
+subtractButton.addEventListener("click", subtract);
+multiplyButton.addEventListener("click", multiply);
+divideButton.addEventListener("click", divide);
 
