6eb6d3d30f960efa7e6d47907c1be2205437dd1e
miuhuru <zermoun.yasmine@gmail.com>
2021-12-25 18:19:05 +0100

    fixed the bug that happened with github merging

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 "use strict";
 
-<<<<<<< HEAD
 const addButton = document.querySelector(".add-button");
 const subtractButton = document.querySelector(".subtract-button");
 const multiplyButton = document.querySelector(".multiply-button");
 const divideButton = document.querySelector(".divide-button");
-=======
-const addButton = document.querySelector(".add-btn");
-const subtractButton = document.querySelector(".subtract-btn");
-const multiplyButton = document.querySelector(".multiply-btn");
-const divideButton = document.querySelector(".divide-btn");
->>>>>>> f67e5a77cd18948d4a8c07c82b0a0c90dfd7ca68
+
 
 const liveDisplay = document.getElementById("display");
 const possibleChoices = document.querySelectorAll(".possibleschoices")
@@ -19,18 +13,7 @@ const possibleChoices = document.querySelectorAll(".possibleschoices")
 let a;
 let b;
 let numberChoice
-<<<<<<< HEAD
-=======
-let one
-let two
-let three
-let four
-let five
-let six
-let seven
-let eight
-let nine
->>>>>>> f67e5a77cd18948d4a8c07c82b0a0c90dfd7ca68
+
 let input_a
 let input_b
 
@@ -66,7 +49,6 @@ function operate(a, b, operator) { // this function takes two number, and an ope
 function getInput() {
    input_a = document.getElementById("number-a").value;
    input_b = document.getElementById("number-b").value;
-<<<<<<< HEAD
   liveDisplay.innerHTML = input_a + " " + input_b 
 }
 
@@ -83,9 +65,8 @@ for (const operator of operators)
     liveDisplay.innerHTML = this.id
   })
 
-=======
   liveDisplay.innerHTML = "Display: " + input_a + " " + input_b 
-}
+
 
 function getNumbers() {
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -101,7 +82,6 @@ function getNumbers() {
   liveDisplay.innerHTML = one + two + three + four + five + six + seven + eight + nine 
 }
 
->>>>>>> f67e5a77cd18948d4a8c07c82b0a0c90dfd7ca68
 addButton.addEventListener("click", add);
 subtractButton.addEventListener("click", subtract);
 multiplyButton.addEventListener("click", multiply);
