d68b53fe08b9c2581568866f1d82ab25e7f1b6b2
miuhuru <zermoun.yasmine@gmail.com>
2022-01-01 21:39:37 +0100

    stuck with the operate() function. can't seem to make it work

--- script.js
+++ script.js
@@ -11,67 +11,76 @@ const possibleChoices = document.querySelectorAll(".possibleschoices");
 const operators = document.querySelectorAll(".operators");
 const numbers = document.querySelectorAll(".numbers");
 
-
 let result;
 let newOperator = "";
 let storedOperator = "";
 let num1 = "";
 let num2 = "";
 
-// const add = (num2, num1) => {
-//   return num2 + num1;
-// };
-// const subtract = (num2, num1) => {
-//   return num2 - num1;
-// };
-// const multiply = (num2, num1) => {
-//   return num2 * num1;
-// };
+const add = (num2, num1) => {
+  return parseFloat(num2 + num1);
+};
+const subtract = (num2, num1) => {
+  return parseFloat(num2 - num1);
+};
+const multiply = (num2, num1) => {
+  return parseFloat(num2 * num1);
+};
 
-// const divide = (num2, num1) => {
-//   if (num2 === 0 || num1 === 0) {
-//     return "Numbers can't be divided by 0. Please try again";
-//   } else return num2 / num1;
-// };
+const divide = (num2, num1) => {
+  if (num2 === 0 || num1 === 0) {
+    return "Numbers can't be divided by 0. Please try again";
+  } else return parseFloat(num2 / num1);
+};
 
 function operate(num2, num1, storedOperator) {
-  if (storedOperator === "+") {
-   result = parseFloat(num2 + num1)
+          if (storedOperator === "+") {
+    result = parseFloat(num2 + num1);
   } else if (storedOperator === "-") {
-   result = parseFloat(num2 - num1)
+    result = parseFloat(num2 - num1);
   } else if (storedOperator === "*") {
-   result = parseFloat(num2 * num1)
+    result = parseFloat(num2 * num1);
   } else if (storedOperator === "/") {
-   result = parseFloat(num2 / num1)
-  }  
-  console.log(result)
-  return mainDisplay.innerText = result;
-}
+    result = parseFloat(num2 / num1) 
+  }
+  }
+  
+function operate(num2, num1, storedOperator) {
+  console.log("operate is fired!")
+    if (storedOperator === "+") {
+      result = parseFloat(num2 + num1);
+    } else if (storedOperator === "-") {
+      result = parseFloat(num2 - num1);
+    } else if (storedOperator === "*") {
+      result = parseFloat(num2 * num1);
+    } else if (storedOperator === "/") {
+      result = parseFloat(num2 / num1)
+    }
+    return result
+  }
+
 
 numbers.forEach((number) => {
   number.addEventListener("click", (e) => {
-    num2 = num1
-    num2 += e.target.innerText
-    mainDisplay.innerText = num2
-  })
-})
+    num2 = num1;
+    num2 += e.target.innerText;
+    mainDisplay.innerText = num2;
+  });
+});
 
 operators.forEach((operator) => {
   operator.addEventListener("click", (e) => {
-    storedOperator = newOperator
-    newOperator = e.target.innerText
-    clearVar()
-  })
-  })
+    storedOperator = newOperator;
+    newOperator = e.target.innerText;
+    operate(num2, num1, storedOperator)
+    clearVar();
+  });
+});
 
 function clearVar() {
-  console.log("clearVal is fired!")
-  operate(num2, num1, storedOperator)
   mainDisplay.innerText = num2;
-  smallDisplay.innerText += num1 + "" + storedOperator + "" + num2
+  smallDisplay.innerText += num1 + "" + storedOperator + "" + num2;
 }
-
-
 
 // i think i want my function to go like this
 
